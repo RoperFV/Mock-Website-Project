2cb03e74f8795b21cb8cdf349a74922adf930a4a
Amaru Loetz <loganloetz@gmail.com>
2022-05-23 18:04:08 -0500

    added nav bar

--- script.js
+++ script.js
@@ -16,3 +16,8 @@ buttons.forEach(button => {
     delete activeSlide.dataset.active
   })
 })
+function menuOnClick() {
+  document.getElementById("menu-bar").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+}
